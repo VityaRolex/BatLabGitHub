f52280c610558ab844e50033065d89871a24238e
VityaRolex <gabarkartoshkin@tuta.io>
2025-08-28 23:34:38 +0300

    .

--- IndividualPractice2025/head.h
+++ IndividualPractice2025/head.h
@@ -5,6 +5,14 @@
 #include <fstream>
 #include <exception>
 
+struct Student{
+    int course;
+    int group;
+    std::string surname;
+    std::string name;
+    std::string patronymic;
+    int marks[3];
+};
 
 
 
