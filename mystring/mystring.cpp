2e9b4dd913aa57848867ce133f5bcfacc32b73d5
VityaRolex <gabarkartoshkin@tuta.io>
2025-10-03 10:10:10 +0300

    .

--- mystring/mystring.cpp
+++ mystring/mystring.cpp
@@ -7,10 +7,7 @@ Mystring& Mystring::operator =(const Mystring& init)
         delete []this->start;
         this->start = new char[init.length];
         this->length = init.length;
-        for(int i = 0; i < length; ++i)
-        {
-            (this->start)[i] = init.start[i];
-        }
+        strcpy(this -> start, init.start);
     }
     return *this;
 }
@@ -22,6 +19,15 @@ bool Mystring::operator ==(const Mystring& rhs)
     return strcmp(this->start, rhs.start) == 0;
 }
 
+int Mystring::operator <=(const Mystring& rhs)
+{
+    return strcmp(this->start, rhs.start) <= 0;
+}
+
+const Mystring Mystring::operator+(const char* rhs)
+{
+    return *this + Mystring(rhs);
+}
 
 const Mystring Mystring::operator +(const Mystring& rhs)
 {
@@ -40,3 +46,75 @@ std::ostream& operator <<(std::ostream& out, const Mystring& a)
     return out;
 }
 
+
+size_t Mystring::find_first_of(const Mystring& search)
+{
+    size_t res{std::string::npos};
+    char* p = strpbrk(this->start, search.start);
+    if(p)
+    {
+        res = p - this->start;
+    }
+    return res;
+}
+
+size_t Mystring::find_first_not_of(const Mystring& search)
+{
+    size_t res{std::string::npos};
+    char* p = this->start;
+    while(p - this->start < this->length)
+    {
+        if(!strchr(search.start, *p))
+        {
+            res =  p - this->start;
+            break;
+        }
+        ++p;
+    }
+
+    return res;
+}
+
+Mystring Mystring::reverse()
+{
+    Mystring res("");
+    size_t length = this->length - 1;
+    res.length = length + 1;
+    char* tmp = new char[2];
+    tmp[1] = '\0';
+    while(length >= 0)
+    {
+        tmp[0] = (this->start)[length];
+        res = res + tmp;
+        --length;
+    }
+    return res;
+}
+
+size_t Mystring::find_last_of(const Mystring& search)
+{
+    Mystring tmp = this->reverse();
+    return tmp.find_first_of(search);
+}
+
+size_t Mystring::find_last_not_of(const Mystring& search)
+{
+    Mystring tmp = this->reverse();
+    return tmp.find_first_of(search);
+}
+
+Mystring Mystring::operator +=(const Mystring& init)
+{
+    return *this + init;
+}
+
+
+Mystring Mystring::substr(size_t length, size_t start = 0)
+{
+    char * res = new char[length];
+    for(int i = 0; i < length; ++i)
+    {
+        res[i] = this->start[start + i];
+    }
+    return Mystring(res);
+}
--- mystring/mystring.h
+++ mystring/mystring.h
@@ -9,7 +9,7 @@ class Mystring
     size_t length;
 public:
 
-Mystring(char* init = nullptr)
+Mystring(const char* init = "")
 {
     this->length = strlen(init);
     this->start = new char[length];
@@ -30,10 +30,15 @@ Mystring& operator =(const Mystring& init);
 const Mystring operator +(const Mystring& rhs);
 friend std::ostream& operator <<(std::ostream& out, const Mystring& a);
 bool operator ==(const Mystring& rhs);
-int operator <=(const Mystring& rhs)
-{
-    return strcmp(this->start, rhs.start) <= 0;
-}
+int operator <=(const Mystring& rhs);
+const Mystring operator+(const char* rhs);
+size_t find_first_of(const Mystring& search);
+size_t find_first_not_of(const Mystring& search);
+Mystring reverse();
+size_t find_last_of(const Mystring& search);
+size_t find_last_not_of(const Mystring& search);
+Mystring operator +=(const Mystring& init);
+Mystring substr(size_t length, size_t start = 0);
 };
 
 
