06686fc26050d065e9eedc8616f76285894e485c
VityaRolex <gabarkartoshkin@tuta.io>
2025-11-21 21:32:02 +0300

    .

--- polymorphism/task1/student.h
+++ polymorphism/task1/student.h
@@ -2,15 +2,17 @@
 #define STUDENT_H
 #include "person.h"
 class Student: public Person{
-int course, group;  //
+int course, group; 
 public:
 Student(const char*,int,int);
 Student(const Student&);
 Student(Student&&);
 Student& operator=(const Student&);
 Student& operator=(Student&&);
-void setCourse(int); void setGroup(int);
-int getCourse()const; int getGroup()const;
+void setCourse(int);
+void setGroup(int);
+int getCourse()const; 
+int getGroup()const;
 void print(std::ostream&) const override;
 Person* clone() const override;
 };
